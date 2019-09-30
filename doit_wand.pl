9fbd72d4946602584c23cd250ee6f23755d59c1c
Mark Allyn <allyn@well.com>
2019-09-29 20:10:03 -0700

    Add colorizing sine wave stuff Confirm to be able to do compile on iverilog

--- doit_wand.pl
+++ doit_wand.pl
@@ -3,7 +3,6 @@ use strict;
 use Math::Trig;
 use File::stat;
 open (my $outstuff, ">", "outstuff.v") or die "cannot open outstuff $!";
-print $outstuff "reg	[0:7] sin[90];\n";
 
 my $index1;
 
@@ -14,16 +13,16 @@ my $myrad;
 my $mysin;
 my $myintsin;
 
-for ($index1 = 0; $index1 < 60; $index1 += 1) {
-  $angle = (360/60) * ($index1 + 15);
+for ($index1 = 0; $index1 < 40; $index1 += 1) {
+  $angle = (360/40) * ($index1);
   $angle = int($angle);
   $myrad = deg2rad($angle);
   $mysin = 1 + sin($myrad);
-  $mysin = $mysin * 100;
+  $mysin = $mysin * 20;
   $myintsin = int($mysin);
   
   print "degree  $index1 - angle $angle radian $myrad - sine $mysin - integer $myintsin\n";
-  print $outstuff "assign sin[$index1] = $myintsin;\n";
+  print $outstuff "assign wand_position_sin[$index1] = $myintsin;\n";
   }
 
 close $outstuff;
--- dostring_wave.v
+++ dostring_wave.v
@@ -40,26 +40,29 @@ localparam TOTAL_CYCLE_SIZE = 60,
 // How far is the lowest part of sine wave from beginning of wand
 SINE_WAVE_BASE = 10,
 
-// How much to devide the sine value to place location of sine wave on wand
-// This is done by shift right; devide by 2 is shift right of 1
-SINE_WAVE_DIVISION = 1,
-
 // Size of each of the color states (descend, flat, and ascend)
-// This is TOTAL_CYCLE_SIZE / 3, which in this case is 20
-COLOR_STATE_SIZE = 20;
+// This is TOTAL_CYCLE_SIZE / 2, which in this case is 30
+COLOR_STATE_SIZE = 30;
+
 
 localparam STRING_SIZE = 47;
 localparam NUMBER_STRINGS = 47;
-localparam MAX_COLOR_VALUE = 100;
+localparam WAND_SINE_SIZE = 40;
+localparam WAND_SINE_BASE = 3;
+
+localparam MAX_COLOR_VALUE = 200;
 
 localparam CREATE_COLOR_TOP = 0,
   CREATE_COLOR_MIDDLE = 1,
   CREATE_COLOR_BOTTOM = 2;
 
-wire[7:0] mysine[TOTAL_CYCLE_SIZE-1:0];
+wire[7:0] color_sin[TOTAL_CYCLE_SIZE-1:0];
+wire[7:0] wand_position_sin[WAND_SINE_SIZE-1:0];
+
 
 reg[7:0] string_iteration_count = 0;
 reg[7:0] create_string_count = 0;
+reg[7:0] wand_sine_count = 0;
 reg[7:0] blue_out = 0;
 reg[7:0] green_out = 0;
 reg[7:0] red_out = 0;
@@ -88,68 +91,114 @@ wire doled_busy;
 assign led1 = mosi;
 assign led2 = dostring_clk;
 
-// This is the sine wave lookup table
+// This is the color sine wave lookup table
+// Range is from 0 to full led level of 200
+// This is not used for position on the wand
 
-assign mysine[0] = 100;
-assign mysine[1] = 99;
-assign mysine[2] = 98;
-assign mysine[3] = 97;
-assign mysine[4] = 95;
-assign mysine[5] = 93;
-assign mysine[6] = 90;
-assign mysine[7] = 87;
-assign mysine[8] = 83;
-assign mysine[9] = 79;
-assign mysine[10] = 75;
-assign mysine[11] = 70;
-assign mysine[12] = 65;
-assign mysine[13] = 60;
-assign mysine[14] = 55;
-assign mysine[15] = 50;
-assign mysine[16] = 44;
-assign mysine[17] = 39;
-assign mysine[18] = 34;
-assign mysine[19] = 29;
-assign mysine[20] = 24;
-assign mysine[21] = 20;
-assign mysine[22] = 16;
-assign mysine[23] = 12;
-assign mysine[24] = 9;
-assign mysine[25] = 6;
-assign mysine[26] = 4;
-assign mysine[27] = 2;
-assign mysine[28] = 1;
-assign mysine[29] = 0;
-assign mysine[30] = 0;
-assign mysine[31] = 0;
-assign mysine[32] = 1;
-assign mysine[33] = 2;
-assign mysine[34] = 4;
-assign mysine[35] = 6;
-assign mysine[36] = 9;
-assign mysine[37] = 12;
-assign mysine[38] = 16;
-assign mysine[39] = 20;
-assign mysine[40] = 24;
-assign mysine[41] = 29;
-assign mysine[42] = 34;
-assign mysine[43] = 39;
-assign mysine[44] = 44;
-assign mysine[45] = 50;
-assign mysine[46] = 55;
-assign mysine[47] = 60;
-assign mysine[48] = 65;
-assign mysine[49] = 70;
-assign mysine[50] = 75;
-assign mysine[51] = 79;
-assign mysine[52] = 83;
-assign mysine[53] = 87;
-assign mysine[54] = 90;
-assign mysine[55] = 93;
-assign mysine[56] = 95;
-assign mysine[57] = 97;
-assign mysine[58] = 98;
-assign mysine[59] = 99;
+assign color_sin[0] = 200;
+assign color_sin[1] = 199;
+assign color_sin[2] = 197;
+assign color_sin[3] = 195;
+assign color_sin[4] = 191;
+assign color_sin[5] = 186;
+assign color_sin[6] = 180;
+assign color_sin[7] = 174;
+assign color_sin[8] = 166;
+assign color_sin[9] = 158;
+assign color_sin[10] = 150;
+assign color_sin[11] = 140;
+assign color_sin[12] = 130;
+assign color_sin[13] = 120;
+assign color_sin[14] = 110;
+assign color_sin[15] = 100;
+assign color_sin[16] = 89;
+assign color_sin[17] = 79;
+assign color_sin[18] = 69;
+assign color_sin[19] = 59;
+assign color_sin[20] = 49;
+assign color_sin[21] = 41;
+assign color_sin[22] = 33;
+assign color_sin[23] = 25;
+assign color_sin[24] = 19;
+assign color_sin[25] = 13;
+assign color_sin[26] = 8;
+assign color_sin[27] = 4;
+assign color_sin[28] = 2;
+assign color_sin[29] = 0;
+assign color_sin[30] = 0;
+assign color_sin[31] = 0;
+assign color_sin[32] = 2;
+assign color_sin[33] = 4;
+assign color_sin[34] = 8;
+assign color_sin[35] = 13;
+assign color_sin[36] = 19;
+assign color_sin[37] = 25;
+assign color_sin[38] = 33;
+assign color_sin[39] = 41;
+assign color_sin[40] = 49;
+assign color_sin[41] = 59;
+assign color_sin[42] = 69;
+assign color_sin[43] = 79;
+assign color_sin[44] = 89;
+assign color_sin[45] = 100;
+assign color_sin[46] = 110;
+assign color_sin[47] = 120;
+assign color_sin[48] = 130;
+assign color_sin[49] = 140;
+assign color_sin[50] = 150;
+assign color_sin[51] = 158;
+assign color_sin[52] = 166;
+assign color_sin[53] = 174;
+assign color_sin[54] = 180;
+assign color_sin[55] = 186;
+assign color_sin[56] = 191;
+assign color_sin[57] = 195;
+assign color_sin[58] = 197;
+assign color_sin[59] = 199;
+
+// Following is the wand position lookup table for placement
+// of the white sine wave on the wand for each iteration
+
+assign wand_position_sin[0] = 20;
+assign wand_position_sin[1] = 23;
+assign wand_position_sin[2] = 26;
+assign wand_position_sin[3] = 29;
+assign wand_position_sin[4] = 31;
+assign wand_position_sin[5] = 34;
+assign wand_position_sin[6] = 36;
+assign wand_position_sin[7] = 37;
+assign wand_position_sin[8] = 39;
+assign wand_position_sin[9] = 39;
+assign wand_position_sin[10] = 40;
+assign wand_position_sin[11] = 39;
+assign wand_position_sin[12] = 39;
+assign wand_position_sin[13] = 37;
+assign wand_position_sin[14] = 36;
+assign wand_position_sin[15] = 34;
+assign wand_position_sin[16] = 31;
+assign wand_position_sin[17] = 29;
+assign wand_position_sin[18] = 26;
+assign wand_position_sin[19] = 23;
+assign wand_position_sin[20] = 20;
+assign wand_position_sin[21] = 16;
+assign wand_position_sin[22] = 13;
+assign wand_position_sin[23] = 10;
+assign wand_position_sin[24] = 8;
+assign wand_position_sin[25] = 5;
+assign wand_position_sin[26] = 3;
+assign wand_position_sin[27] = 2;
+assign wand_position_sin[28] = 0;
+assign wand_position_sin[29] = 0;
+assign wand_position_sin[30] = 0;
+assign wand_position_sin[31] = 0;
+assign wand_position_sin[32] = 0;
+assign wand_position_sin[33] = 2;
+assign wand_position_sin[34] = 3;
+assign wand_position_sin[35] = 5;
+assign wand_position_sin[36] = 8;
+assign wand_position_sin[37] = 10;
+assign wand_position_sin[38] = 13;
+assign wand_position_sin[39] = 16;
 
 // Instantiation of the doled module
 
@@ -185,14 +234,193 @@ always @ (posedge dostring_clk or posedge dostring_reset)
     end
   else
     begin
+    // Outermost state machine; one that handles the SPI to the wand.
+    // All others must nest on this as this one is driven by the SPI
+    // signaling which is slower than the main system clock.
     case (led_send_state)
       STR_WAIT_FOR_LED:
+        // We cannot do much but sit and wait for the SPI to the wand 
         begin
         if (~doled_busy)
           begin
-          led_send_state <= STR_CHECK_COLOR_STATE;
+          led_send_state <= STR_LOAD_COLORS;
           end
         end
+      STR_LOAD_COLORS:
+        begin
+        // This is the 2nd level nesting of state machine. This state machine is driven
+        // by whether we are at the start, end, or anywhere in the middle of the string
+        // sent to the wand. Beginning and ending data sets are special. Those in between
+        // are RGB LED values
+        case (string_state)
+          // Are we at begin, end, or middle of string?
+          STRING_START:
+            begin
+            input_type <= INPUT_TYPE_START;
+            blue_out <= 0;
+            green_out <= 0;
+            red_out <= 0;
+            end
+          STRING_END:
+            begin
+            input_type <= INPUT_TYPE_END;
+            blue_out <= 8'hff;
+            green_out <= 8'hff;
+            red_out <= 8'hff; 
+            end
+          STRING_INSIDE:
+            begin
+            // This is the workhorse. Individual LED colors are set here
+            // The 3rd level nesting of state machine. This is driven by where
+            // we are on the wand; ie; how far are we from the beginning of the wand
+            // Where the handle is glued to the wand itself.
+            // Above the sine wave is between the handle and the white sine wave pattern
+            // On the sine wave is on the white sine wave
+            // Below the sine wave is between the white sine wave and the free end of
+            // the wand.
+            case (string_color_state)
+              // Fourth level nesting state is color sine wave states to determine
+              // individual red/green/blue intensity values for this particular LED
+              STRING_COLOR_TOP: // Between handle and white sine wave
+                begin
+                case (blue_top_sine_state)
+                  // Fifth level nesting state is each individual color sine value
+                  COLOR_ON_ZERO:
+                    begin
+                    blue_out <= 0;
+                    end
+                  COLOR_ASCEND_SINE:
+                    begin
+                    blue_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
+                    end
+                  COLOR_DESCEND_SINE:
+                    begin
+                    blue_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
+                    end
+                  default:
+                    begin
+                    blue_out <= 0;
+                    end
+                  endcase
+                case (green_top_sine_state)
+                  COLOR_ON_ZERO:
+                    begin
+                    green_out <= 0;
+                    end
+                  COLOR_ASCEND_SINE:
+                    begin
+                    green_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
+                    end
+                  COLOR_DESCEND_SINE:
+                    begin
+                    green_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
+                    end
+                  default:
+                    begin
+                    red_out <= 0;
+                    end
+                  endcase
+                case (red_top_sine_state)
+                  COLOR_ON_ZERO:
+                    begin
+                    red_out <= 0;
+                    end
+                  COLOR_ASCEND_SINE:
+                    begin
+                    red_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
+                    end
+                  COLOR_DESCEND_SINE:
+                    begin
+                    red_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
+                    end
+                  default:
+                    begin
+                    red_out <= 0;
+                    end
+                  endcase
+                end
+              STRING_COLOR_BOTTOM:
+                begin
+                case (blue_bottom_sine_state)
+                  COLOR_ON_ZERO:
+                    begin
+                    blue_out <= 0;
+                    end
+                  COLOR_ASCEND_SINE:
+                    begin
+                    blue_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
+                    end
+                  COLOR_DESCEND_SINE:
+                    begin
+                    blue_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
+                    end
+                  default:
+                    begin
+                    blue_out <= 0;
+                    end
+                  endcase
+                case (green_bottom_sine_state)
+                  COLOR_ON_ZERO:
+                    begin
+                    green_out <= 0;
+                    end
+                  COLOR_ASCEND_SINE:
+                    begin
+                    green_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
+                    end
+                  COLOR_DESCEND_SINE:
+                    begin
+                    green_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
+                    end
+                  default:
+                    begin
+                    red_out <= 0;
+                    end
+                  endcase
+                case (red_bottom_sine_state)
+                  COLOR_ON_ZERO:
+                    begin
+                    red_out <= 0;
+                    end
+                  COLOR_ASCEND_SINE:
+                    begin
+                    red_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
+                    end
+                  COLOR_DESCEND_SINE:
+                    begin
+                    red_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
+                    end
+                  default:
+                    begin
+                    red_out <= 0;
+                    end
+                  endcase
+                end
+              STRING_COLOR_MIDDLE:
+                // On the sine wave, all is white
+                begin
+                red_out <= 150;
+                blue_out <= 150;
+                green_out <= 150;
+                end
+              default:
+                begin
+                red_out <= 0;
+                blue_out <= 0;
+                green_out <= 0;
+                end
+              endcase
+            end    
+          default:
+            begin
+            input_type <= INPUT_TYPE_END;
+            blue_out <= 8'hff;
+            green_out <= 8'hff;
+            red_out <= 8'hff;
+            end
+          endcase
+        end 
+            
       endcase
     end
   end
