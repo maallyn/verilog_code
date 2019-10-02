49edd6e5163067f701fc66237e11c0faac0de054
Mark Allyn <allyn@well.com>
2019-10-01 21:26:07 -0700

    Add testing module file dostring_test.v

--- top.v
+++ top.v
@@ -10,41 +10,24 @@ module top (
 
 assign led0 = my_reset;
 
-reg[7:0] counter1 = 0;
-reg local_clock = 0;
 
-always @ (posedge CLK)
-  begin
-  if (counter1 > 3)
-    begin
-    local_clock <= ~local_clock;
-    counter1 <= 0;
-    end
-  else
-    begin
-    // local_clock <= ~local_clock; // Debugging
-    counter1 <= counter1 + 1;
-    end
-  end
-    
-
-//clk_wiz_0 my_main_clock
-  //(
+clk_wiz_0 my_main_clock
+  (
    //Clock out ports  
-  //.clk_out1(clk_out1),
+  .clk_out1(clk_out1),
   // Status and control signals               
-  //.reset(my_reset), 
-  //.locked(my_locked),
+  .reset(my_reset), 
+  .locked(my_locked),
  // Clock in ports
-  //.clk_in1(CLK)
- //);
+  .clk_in1(CLK)
+ );
 
 dostring_wave dostring_wave1 (
   .led1(led1),
   .led2(led2),
   .mosi(mosi),
   .sck(sck),
-  .dostring_reset(my_reset),
-  .dostring_clk(local_clock)
+  .dostring_reset(~my_locked),
+  .dostring_clk(clk_out1)
 );
 endmodule
