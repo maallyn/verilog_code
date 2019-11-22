247a61424cef802efb53b22440caf49865304eb6
Mark Allyn <allyn@well.com>
2019-11-22 08:45:23 -0800

    Working on waterfall rainbow

--- dostring_wave.v
+++ dostring_wave.v
@@ -21,11 +21,6 @@ module dostring_wave (
   input wire dostring_clk
 );
 
-// Start point for ascend in sine wave and descend in sine wave
-localparam
-  SINE_ASCEND_START_INDEX = 30,
-  SINE_DESCEND_START_INDEX = 0;
-
 // Each string needs special data at the beginning and the end, otherwise
 // data is tri-color data for each led in the string
 localparam
@@ -44,37 +39,27 @@ localparam
   STR_CHECK_COUNTERS = 3,
   STR_CLEAR_START = 4,
   STR_INCREMENT_WAND_COUNTERS = 5,
-  STR_SET_MIDPOINT_AND_COLORS = 6;
+  STR_SET_THE_SEGMENT_TO_START_SEGMENT = 6;
 
-// This is the state of each color in the sine wave. It can ascend via sine,
-// descend via sine, or stay at zero. For example, at the violet end of the 
-// rainbow, green is staying flat, blue is ascending, and red is descending;
-// blue in rainbow is red staying flat, blue at peak and staring to descend
-// and green is starting to ascend from staying flat at zero,
-// then we start to move into cyan with blue descending, red staying flat at zero,
-// and green ascending;
-// and so on throughout the rainbow until we get to red (red at full; green starts
-// flatline at zero, and blue starts to ascend while red starts to descend, which
-// means we are starting the rainbow at violet once again
-localparam
-  COLOR_ON_ZERO = 0,
-  COLOR_ASCEND_SINE = 1,
-  COLOR_DESCEND_SINE = 2;
+// Which color segment are we on?
+// Goes from 0 for purple to COLOR_SEGMENT_NUMBER for red
+reg[7:0] color_segment_count;
 
-// Where are we in the current stage (each stage is 30 steps)
-reg[7:0] color_stage_count;
+// Where in color segment are we?
+// Goes from 0 to COLOR_SEGMENT_SIZE
+reg[7:0] color_segment_position;
+
+// Where in the segment shall we start this string
+reg[7:0] start_in_segment;
+
+// Which segment to start in
+reg[7:0] which_segment_to_start;
 
 // Begin and end are special data; inside is normal tri color data
 reg[1:0] string_state;
 
 // LED SPI data rate is low, we need to wait, then load data, then start send
 reg[2:0] led_send_state;
-
-// Following three are for the sine wave color state for each respective
-// primary color for color above sine wave
-reg[1:0] blue_top_sine_state;
-reg[1:0] green_top_sine_state;
-reg[1:0] red_top_sine_state; 
 
 // This is for the led output module. There are three data types:
 // First of string is a special start string data set,
@@ -85,25 +70,22 @@ localparam
   INPUT_TYPE_LED = 1,
   INPUT_TYPE_END = 2;
 
-// Total length of color sine wave
-localparam TOTAL_CYCLE_SIZE = 60,
-
-// Size of each of the color states (descend, flat, and ascend)
-// This is TOTAL_CYCLE_SIZE / 2, which in this case is 30
-COLOR_STATE_SIZE = 30;
-
-
 localparam STRING_SIZE = 47;
-localparam NUMBER_STRINGS = 47;
-localparam WAND_SINE_SIZE = 40; // Length of sine wave in iterations
-localparam WAND_SINE_BASE = 3; // Distance from bottome of sine wave to handle
+
+localparam
+  COLOR_FULL_BRITE = 8'hf0,
+  COLOR_QUARTER_BRITE = 8'h40,
+  COLOR_HALF_BRITE = 8'h80,
+  COLOR_MIN_BRITE = 9'h02,
+  COLOR_THREE_QUARTER_BRITE = 8'hc0,
+  COLOR_SEGMENT_SIZE = 10,
+  COLOR_SEGMENT_NUMBER = 7;
 
 localparam MAX_COLOR_VALUE = 200;
 
-wire[7:0] color_sin[TOTAL_CYCLE_SIZE-1:0];
-
-// What String are we on
-reg[7:0] string_iteration_count = 0;
+wire[7:0] blue_seg[COLOR_SEGMENT_NUMBER-1:0];
+wire[7:0] green_seg[COLOR_SEGMENT_NUMBER-1:0];
+wire[7:0] red_seg[COLOR_SEGMENT_NUMBER-1:0];
 
 // Where in the string are we? This will repeat for each scan of the
 // wand. Zero is at the joint between the wand and the handle.
@@ -135,70 +117,41 @@ wire doled_busy;
 assign led1 = mosi;
 assign led2 = sck;
 
-// This is the color sine wave lookup table
-// Range is from 0 to full led level of 200
-// This is not used for position on the wand
+// Rainbow color values for color arrays
+// purple
+assign blue_seg[0] = COLOR_HALF_BRITE;
+assign green_seg[0] = COLOR_MIN_BRITE;
+assign red_seg[0] = COLOR_HALF_BRITE;
 
-assign color_sin[0] = 200;
-assign color_sin[1] = 199;
-assign color_sin[2] = 197;
-assign color_sin[3] = 195;
-assign color_sin[4] = 191;
-assign color_sin[5] = 186;
-assign color_sin[6] = 180;
-assign color_sin[7] = 174;
-assign color_sin[8] = 166;
-assign color_sin[9] = 158;
-assign color_sin[10] = 150;
-assign color_sin[11] = 140;
-assign color_sin[12] = 130;
-assign color_sin[13] = 120;
-assign color_sin[14] = 110;
-assign color_sin[15] = 100;
-assign color_sin[16] = 89;
-assign color_sin[17] = 79;
-assign color_sin[18] = 69;
-assign color_sin[19] = 59;
-assign color_sin[20] = 49;
-assign color_sin[21] = 41;
-assign color_sin[22] = 33;
-assign color_sin[23] = 25;
-assign color_sin[24] = 19;
-assign color_sin[25] = 13;
-assign color_sin[26] = 8;
-assign color_sin[27] = 4;
-assign color_sin[28] = 2;
-assign color_sin[29] = 0;
-assign color_sin[30] = 0;
-assign color_sin[31] = 0;
-assign color_sin[32] = 2;
-assign color_sin[33] = 4;
-assign color_sin[34] = 8;
-assign color_sin[35] = 13;
-assign color_sin[36] = 19;
-assign color_sin[37] = 25;
-assign color_sin[38] = 33;
-assign color_sin[39] = 41;
-assign color_sin[40] = 49;
-assign color_sin[41] = 59;
-assign color_sin[42] = 69;
-assign color_sin[43] = 79;
-assign color_sin[44] = 89;
-assign color_sin[45] = 100;
-assign color_sin[46] = 110;
-assign color_sin[47] = 120;
-assign color_sin[48] = 130;
-assign color_sin[49] = 140;
-assign color_sin[50] = 150;
-assign color_sin[51] = 158;
-assign color_sin[52] = 166;
-assign color_sin[53] = 174;
-assign color_sin[54] = 180;
-assign color_sin[55] = 186;
-assign color_sin[56] = 191;
-assign color_sin[57] = 195;
-assign color_sin[58] = 197;
-assign color_sin[59] = 199;
+// blue
+assign blue_seg[0] = COLOR_FULL_BRITE;
+assign green_seg[0] = COLOR_MIN_BRITE;
+assign red_seg[0] = COLOR_MIN_BRITE;
+
+// cyan
+assign blue_seg[0] = COLOR_HALF_BRITE;
+assign green_seg[0] = COLOR_HALF_BRITE;
+assign red_seg[0] = COLOR_MIN_BRITE;
+
+// green
+assign blue_seg[0] = COLOR_MIN_BRITE;
+assign green_seg[0] = COLOR_FULL_BRITE;
+assign red_seg[0] = COLOR_MIN_BRITE;
+
+// yellow
+assign blue_seg[0] = COLOR_MIN_BRITE;
+assign green_seg[0] = COLOR_HALF_BRITE;
+assign red_seg[0] = COLOR_FULL_BRITE;
+
+// orange
+assign blue_seg[0] = COLOR_MIN_BRITE;
+assign green_seg[0] = COLOR_QUARTER_BRITE;
+assign red_seg[0] = COLOR_THREE_QUARTER_BRITE;
+
+// red
+assign blue_seg[0] = COLOR_MIN_BRITE;
+assign green_seg[0] = COLOR_MIN_BRITE;
+assign red_seg[0] = COLOR_FULL_BRITE;
 
 // Instantiation of the doled module
 
@@ -224,13 +177,12 @@ always @ (posedge dostring_clk or posedge dostring_reset)
     red_out <= 0;
     green_out <= 0;
     led_start <= 0;
-    color_stage_count <= 0;
     string_state <= STRING_START;
     led_send_state <= STR_WAIT_FOR_LED;
-    blue_top_sine_state <= COLOR_DESCEND_SINE;
-    red_top_sine_state <= COLOR_ON_ZERO;
-    green_top_sine_state <= COLOR_ASCEND_SINE;
-    create_string_count <= 0;
+    color_segment_count <= 0;
+    color_segment_position <= 0;
+    start_in_segment <= 0;
+    which_segment_to_start <= 0;
     end
   else
     begin
@@ -278,84 +230,19 @@ always @ (posedge dostring_clk or posedge dostring_reset)
             // The 3rd level nesting of state machine. This is driven by where
             // we are on the wand; ie; how far are we from the beginning of the wand
             // Where the handle is glued to the wand itself.
-            // Above the sine wave is between the handle and the white sine wave pattern
-            // On the sine wave is on the white sine wave
-            // Below the sine wave is between the white sine wave and the free end of
-            // the wand.
-            // Where we are in the overall color rainbow also determines what we send
-            // to the led. The color rainbow position is the same for the entire string;
-            // it changes only between strings.
+            // Where we are in a segment and which segment we are in determine the
+            // color values
+ 
+            // set the color values
+            blue_out <= blue_seg[color_segment_count];
+            green_out <= green_seg[color_segment_count];
+            red_out <= red_seg[color_segment_count];
+
+            // Bump up color_segment_position
+            color_segment_position <= color_segment_position + 1;
+
+            // This is the input type
             input_type <= INPUT_TYPE_LED;
-
-            case (blue_top_sine_state)
-              COLOR_ON_ZERO:
-                begin
-                blue_out <= 0;
-                end
-
-              COLOR_ASCEND_SINE:
-                begin
-                blue_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
-                end
-
-              COLOR_DESCEND_SINE:
-                begin
-                blue_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
-                end
-
-              default:
-                begin
-                blue_out <= 0;
-                end
-
-              endcase // blue_top_sine_state
-
-            case (green_top_sine_state)
-              COLOR_ON_ZERO:
-                begin
-                green_out <= 0;
-                end
-
-              COLOR_ASCEND_SINE:
-                begin
-                green_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
-                end
-
-              COLOR_DESCEND_SINE:
-                begin
-                green_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
-                end
-
-              default:
-                begin
-                green_out <= 0;
-                end
-
-              endcase // green_top_sine_state
-
-            case (red_top_sine_state)
-              COLOR_ON_ZERO:
-                begin
-                red_out <= 0;
-                end
-
-              COLOR_ASCEND_SINE:
-                begin
-                red_out <= color_sin[SINE_ASCEND_START_INDEX + color_stage_count];
-                end
-
-              COLOR_DESCEND_SINE:
-                begin
-                red_out <= color_sin[SINE_DESCEND_START_INDEX + color_stage_count];
-                end
-
-              default:
-                begin
-                red_out <= 0;
-                end
-
-              endcase // red_top_sine_state
-
             end
 
           default:
@@ -374,21 +261,7 @@ always @ (posedge dostring_clk or posedge dostring_reset)
       STR_SEND_COLORS:
         begin
         led_start <= 1; // Tell SPI to do it's thing
-        // While we are at it, we can increment the create_string_count.
-        // We do not increment the color_stage_count 
-        // because it stays for the entire duration of the string;
-        // it is incremented and checked only at the end of the entire string
-        if (create_string_count < STRING_SIZE)
-          begin
-          create_string_count <= create_string_count + 1;
-          end
-        else
-          begin
-          create_string_count <= 0;
-          end
 
-        // We will do checking and state machine adjustment at the next
-        // clock cycle
         led_send_state <= STR_CHECK_COUNTERS;
         end
 
@@ -402,6 +275,25 @@ always @ (posedge dostring_clk or posedge dostring_reset)
         else if (create_string_count < STRING_SIZE)
           begin
           string_state <= STRING_INSIDE;
+          // and while we are still inside the string, check the segment position
+          // and bump up segment if we are at the end of the segment
+          if (color_segment_position >= COLOR_SEGMENT_SIZE)
+            begin
+            // Are we at the last segment? If so, go back to the start of the rainbow
+            if (color_segment_count >= COLOR_SEGMENT_NUMBER)
+              begin
+              color_segment_count <= 0;
+              end
+            else
+              begin
+              color_segment_count <= color_segment_count + 1;
+              end
+            color_segment_position <= 0;
+            end
+          else
+            begin
+            color_segment_position <= color_segment_position + 1;
+            end
           end
         else
           begin
@@ -417,8 +309,9 @@ always @ (posedge dostring_clk or posedge dostring_reset)
         // with it
         begin
         led_start <= 0;
+
         if (string_state == STRING_END)
-          // At end of string, rotate colors, and set the new middle point,
+          // At end of string, set the next string start segment and position
           // otherwise wait for SPI to finish and then output the next LED
           begin
           led_send_state <= STR_INCREMENT_WAND_COUNTERS;
@@ -431,64 +324,32 @@ always @ (posedge dostring_clk or posedge dostring_reset)
 
       STR_INCREMENT_WAND_COUNTERS:
         begin
-        color_stage_count <= color_stage_count + 1;
-        led_send_state <= STR_SET_MIDPOINT_AND_COLORS;
+        // Now determine the new segment start position and which segment
+        if (start_in_segment >= COLOR_SEGMENT_SIZE)
+          begin
+          // Switch segment to start in
+          if (which_segment_to_start >= COLOR_SEGMENT_NUMBER)
+            begin
+            // Go back to beginning of rainbow
+            which_segment_to_start <= 0;
+            end
+          else
+            begin
+            which_segment_to_start <= which_segment_to_start + 1;
+            end
+          start_in_segment <= 0;
+          end
+        else
+          begin
+          start_in_segment <= start_in_segment + 1;
+          end
+        led_send_state <= STR_SET_THE_SEGMENT_TO_START_SEGMENT;
         end
 
-      STR_SET_MIDPOINT_AND_COLORS:
+      STR_SET_THE_SEGMENT_TO_START_SEGMENT:
         begin
-        // Now the challenging part, update color sequence states if at
-        // the end of a color segment
-        if (color_stage_count >= COLOR_STATE_SIZE)
-          begin
-          // first, clear the counter
-          color_stage_count <= 0;
-
-          // do top states first
-
-          // blue
-          if (blue_top_sine_state == COLOR_ON_ZERO)
-            begin
-            blue_top_sine_state <= COLOR_ASCEND_SINE;
-            end
-          else if (blue_top_sine_state == COLOR_ASCEND_SINE)
-            begin
-            blue_top_sine_state <= COLOR_DESCEND_SINE;
-            end
-          else
-            begin
-            blue_top_sine_state <= COLOR_ON_ZERO;
-            end
-          
-          // green
-          if (green_top_sine_state == COLOR_ON_ZERO)
-            begin
-            green_top_sine_state <= COLOR_ASCEND_SINE;
-            end
-          else if (green_top_sine_state == COLOR_ASCEND_SINE)
-            begin
-            green_top_sine_state <= COLOR_DESCEND_SINE;
-            end
-          else
-            begin
-            green_top_sine_state <= COLOR_ON_ZERO;
-            end
-          
-          // red
-          if (red_top_sine_state == COLOR_ON_ZERO)
-            begin
-            red_top_sine_state <= COLOR_ASCEND_SINE;
-            end
-          else if (red_top_sine_state == COLOR_ASCEND_SINE)
-            begin
-            red_top_sine_state <= COLOR_DESCEND_SINE;
-            end
-          else
-            begin
-            red_top_sine_state <= COLOR_ON_ZERO;
-            end
-  
-          end
+        color_segment_position <= start_in_segment;
+        color_segment_count <= which_segment_to_start;
         led_send_state <= STR_WAIT_FOR_LED;
         end  
 
